095aefe2b2d52bbd04aeafcd5c0dbbed9d965367
Tom Densham <tom.densham@studiobonito.co.uk>
2013-05-10 09:52:17 +0100

    ADD: Replace _config.php with new _config/config.yml

--- _config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-Image::add_extension('ImageFunctionsExtension');
-
-Page::add_extension('PlaceholderImagePageExtension');
-SiteConfig::add_extension('PlaceholderImageSiteConfigExtension');
-
-GD::set_default_quality(100);
